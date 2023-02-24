55496fb4eee1e0860cae3e30747f1db03b7168da
Zuzanna Konieczna <zuzannakonieczna29@gmail.com>
2023-02-25 00:45:20 +0100

    menu

---
+++ Assets/Menu/buttons.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttons : MonoBehaviour
+{
+    public void Play(){
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit(){
+        Application.Quit();
+    }
+}
